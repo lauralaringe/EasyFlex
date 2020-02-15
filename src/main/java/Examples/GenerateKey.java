f7e9ae02a5b9a8df425239bb3e808a3fb3bdc15a
lauralarn <lauralarin@hotmail.it>
2020-02-15 22:09:40 +0100

    First Commit

---
+++ src/main/java/Examples/GenerateKey.java
@@ -0,0 +1,17 @@
+package Examples;
+
+import com.hedera.hashgraph.sdk.crypto.ed25519.Ed25519PrivateKey;
+import com.hedera.hashgraph.sdk.crypto.ed25519.Ed25519PublicKey;
+
+public final class GenerateKey {
+    private GenerateKey() { }
+
+    public static void main(String[] args) {
+        // Generate a Ed25519 private, public key pair
+        Ed25519PrivateKey newKey = Ed25519PrivateKey.generate();
+        Ed25519PublicKey newPublicKey = newKey.publicKey;
+
+        System.out.println("private key = " + newKey);
+        System.out.println("public key = " + newPublicKey);
+    }
+}
