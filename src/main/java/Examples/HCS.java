a7d3cbcce35ad805cfc33afa12472870f646353e
lauralarn <lauralarin@hotmail.it>
2020-03-03 21:08:37 +0100

    Added HCS, modified env and added xml

--- src/main/java/Examples/HCS.java
+++ src/main/java/Examples/HCS.java
@@ -1,33 +1,31 @@
 package Examples;
 
 import com.hedera.hashgraph.sdk.Client;
-import com.hedera.hashgraph.sdk.HederaNetworkException;
 import com.hedera.hashgraph.sdk.HederaStatusException;
 import com.hedera.hashgraph.sdk.TransactionId;
 import com.hedera.hashgraph.sdk.account.AccountId;
-import com.hedera.hashgraph.sdk.consensus.ConsensusMessageSubmitTransaction;
 import com.hedera.hashgraph.sdk.consensus.ConsensusTopicCreateTransaction;
 import com.hedera.hashgraph.sdk.consensus.ConsensusTopicId;
 import com.hedera.hashgraph.sdk.crypto.ed25519.Ed25519PrivateKey;
-
 import com.hedera.hashgraph.sdk.mirror.MirrorClient;
-import com.hedera.hashgraph.sdk.mirror.MirrorConsensusTopicQuery;
 import io.github.cdimascio.dotenv.Dotenv;
 
-import java.nio.charset.StandardCharsets;
 import java.util.Objects;
+
 
 public class HCS {
 
     // Grab the OPERATOR_ID and OPERATOR_KEY from the .env file
     private static final AccountId OPERATOR_ID = AccountId.fromString(Objects.requireNonNull(Dotenv.load().get("OPERATOR_ID")));
+
     private static final Ed25519PrivateKey OPERATOR_KEY = Ed25519PrivateKey.fromString(Objects.requireNonNull(Dotenv.load().get("OPERATOR_KEY")));
 
     // Grab the mirror node endpoint from the .env file
     private static final String MIRROR_NODE_ADDRESS = Objects.requireNonNull(Dotenv.load().get("MIRROR_NODE_ADDRESS"));
 
+    private HCS() { }
 
-    public static void main(String[] args) throws HederaStatusException, HederaNetworkException {
+    public static void main(String[] args) {
 
         // Build Hedera testnet client
         Client client = Client.forTestnet();
@@ -37,15 +35,23 @@ public class HCS {
 
         // Build the mirror node client
         final MirrorClient mirrorClient = new MirrorClient(MIRROR_NODE_ADDRESS);
+
         //Create a new topic
+        final TransactionId transactionId;
+        try {
+            transactionId = new ConsensusTopicCreateTransaction()
+                    .execute(client);
 
-        final TransactionId transactionId = new ConsensusTopicCreateTransaction()
-                .execute(client);
+            //Grab the newly generated topic ID
+            final ConsensusTopicId topicId = transactionId.getReceipt(client).getConsensusTopicId();
 
-//Grab the newly generated topic ID
-        final ConsensusTopicId topicId = transactionId.getReceipt(client).getConsensusTopicId();
+            System.out.println("Your topic ID is: " +topicId);
+        } catch (HederaStatusException e) {
+            e.printStackTrace();
+        }
 
-        System.out.println("Your topic ID is: " +topicId);
+
+
 
 
 
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,25 @@ public class Main {
 
 
     public static void main(String[] args) {
-        // Code goes here
+
+// create UI/GUI
+
+
+//create class user with parameters that will serve for flexibility check (power etc.)
+
+// set up for User: create account, generate key
+//in create account you generate the ID
+//insert parameters of class User
+
+
+//check if the new user can be a flexibility resource
+
+
+
+//if it can be a flexibility resource --> add to class
+
+
+
     }
 
 
