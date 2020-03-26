66607718d190ec54fa0fa8ad09f610818ada08f0
oadamanik <61023933+oadamanik@users.noreply.github.com>
2020-03-26 13:34:21 +0100

    Added ImportMeas and Transaction

---
+++ src/main/java/ImportMeas.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ImportMeas {
+   String file;
+   List<String> content;
+    public void setData(String file){
+        this.file=file;
+        List<String> content = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                content.add(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        this.content=content;
+    }
+    public List<String> getData(){
+        return content;
+    }
+    public void printData(){
+        System.out.println(getData());
+    }
+}
+
